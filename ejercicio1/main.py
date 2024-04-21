702eaea1833bebd6a52aa49494785781c3f61ae8
josephdeltoro2001 <158236768+josephdeltoro2001@users.noreply.github.com>
2024-04-21 00:35:43 -0700

    Ejercicio 1

---
+++ ejercicio1/main.py
@@ -0,0 +1,21 @@
+from operaciones_matematicas import *
+
+#Variables de numeros que se utilizaran para las operaciones
+num1 = 56
+num2 = 19
+
+#Suma
+resultado_suma = suma(num1, num2)
+print(f"Suma: {num1} + {num2} = {resultado_suma}")
+
+#Resta
+resultado_resta = resta(num1, num2)
+print(f"Resta: {num1} - {num2} = {resultado_resta}")
+
+#Multiplicación
+resultado_multiplicacion = multiplicacion(num1, num2)
+print(f"Multiplicación: {num1} * {num2} = {resultado_multiplicacion}")
+
+#División
+resultado_division = division(num1, num2)
+print(f"División: {num1} / {num2} = {resultado_division}")
---
+++ ejercicio1/operaciones_matematicas.py
@@ -0,0 +1,16 @@
+# operaciones_matematicas.py
+
+def suma(numero1, numero2):
+    return numero1 + numero2
+
+def resta(numero1, numero2):
+    return numero1 - numero2
+
+def multiplicacion(numero1, numero2):
+    return numero1 * numero2
+
+def division(numero1, numero2):
+    if numero2 != 0:
+        return numero1 / numero2
+    else:
+        return "No es posible dividir por cero."
