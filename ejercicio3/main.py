8f4aa3608f381bdb9bebe0e2353c354a4ce5578f
josephdeltoro2001 <158236768+josephdeltoro2001@users.noreply.github.com>
2024-04-21 00:38:04 -0700

    Ejercicio 3

---
+++ ejercicio3/main.py
@@ -0,0 +1,15 @@
+#Ejemplo con sqrt
+from math import sqrt
+numero = 30
+raiz_cuadrada = sqrt(numero)
+
+print("Raíz cuadrada con sqrt")
+print(f"La raíz cuadrada de {numero} es: {raiz_cuadrada}\n")
+
+#Ejemplo con un alias
+import math as m
+numero = 40
+raiz_cuadrada2 = m.sqrt(numero)
+
+print("Raíz cuadrada con un alias")
+print(f"La raíz cuadrada de {numero} es: {raiz_cuadrada2}")
